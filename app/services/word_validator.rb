2423794352bcecb8be5a4b7d572bb311428c945b
Eliot Swank <eliotswank@gmail.com>
2018-09-12 11:31:11 -0600

    continues work on word_validator.  Returning 404 for plural word

--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -12,8 +12,6 @@ class WordValidator
 
   def find_word
     headers = {'app_id' => ENV['OED_APP_ID'], 'app_key' => ENV['OED_API_KEY']}
-    # @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
-    # end
     response = @conn.get {|request| request.headers = headers}
 
     binding.pry
