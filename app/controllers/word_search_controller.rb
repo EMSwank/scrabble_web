d14bda5da3b5d32d8ef32da4d186f227d3a60770
Eliot Swank <eliotswank@gmail.com>
2018-09-12 10:38:20 -0600

    work on authenticating api request

--- app/controllers/word_search_controller.rb
+++ app/controllers/word_search_controller.rb
@@ -1,6 +1,7 @@
 class WordSearchController < ApplicationController
   def create
     @validator = WordValidator.new(params[:word_search])
+    @validator.find_word
     redirect_to root_path
   end
 end
--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -1,5 +1,21 @@
 class WordValidator
-  def initialize(word_params)
-    @word_params = word_params
+  def initialize(word)
+    @word = word
+    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
   end
+
+  def find_word 
+    headers = {'app_id' => ENV['OED_APP_ID'], 'api_key' => ENV['OED_API_KEY']}
+    response = @conn.get do |request|
+      request.headers = headers
+    end
+    JSON.parse(response.body, headers: true)
+    
+    binding.pry
+    
+  end
+
+
 end
