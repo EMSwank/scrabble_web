d6b9d0761455f7f632bc8b7dab129f7a6f22e853
Eliot Swank <eliotswank@gmail.com>
2018-09-12 10:59:41 -0600

    working on "Authentication parameters missing" error on API get request

--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -1,20 +1,19 @@
 class WordValidator
+  attr_reader :find_word
+
   def initialize(word)
     @word = word
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
-      faraday.adapter  Faraday.default_adapter
-    end
   end
 
-  def find_word 
-    headers = {'app_id' => ENV['OED_APP_ID'], 'api_key' => ENV['OED_API_KEY']}
-    response = @conn.get do |request|
-      request.headers = headers
+  def find_word
+    headers = {'app_id' => ENV['OED_APP_ID'], 'api_id' => ENV['OED_API_KEY']}
+    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
+      faraday.headers = headers
+      faraday.adapter  Faraday.default_adapter
     end
-    JSON.parse(response.body, headers: true)
-    
+    response = @conn.get
     binding.pry
-    
+    JSON.parse(response.body, headers: true)
   end
 
 
