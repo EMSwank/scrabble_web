bdf48b68e6e4068ec25bbeefd3d54dd3d6a8ce08
Eliot Swank <eliotswank@gmail.com>
2018-09-12 11:36:45 -0600

    fixed request due to misreading the docs--had wrong API path

--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -3,7 +3,7 @@ class WordValidator
 
   def initialize(word)
     @word = word
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}") do |faraday|
+    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word}") do |faraday|
       faraday.headers["app_id"] = ENV["OED_APP_ID"]
       faraday.headers["api_id"] = ENV["OED_API_ID"]
       faraday.adapter  Faraday.default_adapter
@@ -13,8 +13,6 @@ class WordValidator
   def find_word
     headers = {'app_id' => ENV['OED_APP_ID'], 'app_key' => ENV['OED_API_KEY']}
     response = @conn.get {|request| request.headers = headers}
-
-    binding.pry
     JSON.parse(response.body, headers: true)
   end
 
