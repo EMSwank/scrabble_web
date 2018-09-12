fc802d157d9232b6f002fbe80479215aef33d77d
Eliot Swank <eliotswank@gmail.com>
2018-09-12 11:13:04 -0600

    continue work on OED API get request--coming back nil

--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -8,10 +8,9 @@ class WordValidator
   def find_word
     headers = {'app_id' => ENV['OED_APP_ID'], 'api_id' => ENV['OED_API_KEY']}
     @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
-      faraday.headers = headers
-      faraday.adapter  Faraday.default_adapter
     end
-    response = @conn.get
+    response = @conn.get {|request| request.headers = headers}
+
     binding.pry
     JSON.parse(response.body, headers: true)
   end
