1e4289b5e732f8d8c3928483d169c30f3a48fa8a
Eliot Swank <eliotswank@gmail.com>
2018-09-12 11:20:56 -0600

    API working--response for word search is 'not found'

--- app/controllers/word_search_controller.rb
+++ app/controllers/word_search_controller.rb
@@ -1,7 +1,8 @@
 class WordSearchController < ApplicationController
   def create
-    @validator = WordValidator.new(params[:word_search])
-    @validator.find_word
+    validator = WordValidator.new(params[:word_search])
+    validator.find_word
+
     redirect_to root_path
   end
 end
--- app/services/word_validator.rb
+++ app/services/word_validator.rb
@@ -3,12 +3,17 @@ class WordValidator
 
   def initialize(word)
     @word = word
+    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}") do |faraday|
+      faraday.headers["app_id"] = ENV["OED_APP_ID"]
+      faraday.headers["api_id"] = ENV["OED_API_ID"]
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 
   def find_word
-    headers = {'app_id' => ENV['OED_APP_ID'], 'api_id' => ENV['OED_API_KEY']}
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
-    end
+    headers = {'app_id' => ENV['OED_APP_ID'], 'app_key' => ENV['OED_API_KEY']}
+    # @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{@word}") do |faraday|
+    # end
     response = @conn.get {|request| request.headers = headers}
 
     binding.pry
